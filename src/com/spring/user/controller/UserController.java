f532625fc9f00a7097dee2299d4d9dcf0f03bd47
choijinhap <wlsgkq123@gmail.com>
2020-02-19 17:04:21 +0900

    user 컨트롤러,서비스,다오 작성해야함

---
+++ src/com/spring/user/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.spring.user.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.board.HomeController;
+import com.spring.board.service.boardService;
+import com.spring.board.vo.BoardVo;
+import com.spring.board.vo.ComcodeVo;
+import com.spring.board.vo.PageVo;
+import com.spring.common.CommonUtil;
+
+public class UserController {
+
+}
